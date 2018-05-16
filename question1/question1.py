8cb7150db82050055f3e1ec97e8f3e59440d29bf
Mathew Bub <mathew.bub@gmail.com>
2018-05-16 12:36:32 -0400

    Moved Gaia query code into a function.

--- question1/question1.py
+++ question1/question1.py
@@ -3,42 +3,35 @@ from astropy.coordinates import SkyCoord
 from astroquery.simbad import Simbad
 from astroquery.gaia import Gaia
 
-# Note: convert this program into a function.
+def star_to_orbit(star_name, radius = 1.0*u.arcsec):
+    # Get data on the star from SIMBAD, for use in epoch adjustment
+    custom_simbad = Simbad()
+    custom_simbad.add_votable_fields('pmra', 'pmdec', 'plx', 'rv_value')
+    simbad_table = custom_simbad.query_object(star_name)
+    simbad_coord = SkyCoord(simbad_table['RA'][0], simbad_table['DEC'][0], 
+                            unit = (u.hourangle, u.deg))
+    simbad_vals = (simbad_coord.ra.value,
+                   simbad_coord.dec.value,
+                   simbad_table['PLX_VALUE'][0],
+                   simbad_table['PMRA'][0],
+                   simbad_table['PMDEC'][0],
+                   simbad_table['RV_VALUE'][0])
+    
+    # Circle radius to query Gaia
+    radius_deg = radius.to(u.deg)
+    
+    # Perform a cone search with epoch adjustment
+    query =  """
+             SELECT source_id, ra, dec, pmra, pmdec, parallax, radial_velocity
+             FROM gaiadr2.gaia_source
+             WHERE 1=CONTAINS(
+             POINT('ICRS',gaiadr2.gaia_source.ra,gaiadr2.gaia_source.dec),
+             CIRCLE('ICRS', 
+             COORD1(EPOCH_PROP_POS({0},{1},{2},{3},{4},{5},2000,2015.5)),
+             COORD2(EPOCH_PROP_POS({0},{1},{2},{3},{4},{5},2000,2015.5)), {6}))
+             """.format(*simbad_vals, radius_deg.value)
+             
+    # Perfrom the cone search and get the result in a table
+    job = Gaia.launch_job_async(query)
+    return job.get_results()
 
-# Stars to query
-stars = ["GJ 440", "Lacaille 8760", "Vega"]
-
-# Get data on each star from SIMBAD, for use in epoch adjustment
-customSimbad = Simbad()
-customSimbad.add_votable_fields('pmra', 'pmdec', 'plx', 'rv_value')
-simbad_table = customSimbad.query_objects(stars)
-
-# Convert ra and dec from SIMBAD into SkyCoord objects for each star
-simbad_coords = [SkyCoord(simbad_table['RA'][i], simbad_table['DEC'][i], 
-                   unit = (u.hourangle, u.deg)) for i in range(len(stars))]
-
-# Hold the data on each star in a tuple of the form 
-# (ra, dec, parallax, pmra, pmdec, radial_velocity)
-simbad_vals = [(simbad_coords[i].ra.value, simbad_coords[i].dec.value, 
-                simbad_table['PLX_VALUE'][i], simbad_table['PMRA'][i], 
-                simbad_table['PMDEC'][i], simbad_table['RV_VALUE'][i])
-                for i in range(len(stars))]
-
-# Choose a circle radius to query in Gaia
-radius = (1.0*u.arcsec).to(u.deg)
-
-# Perform a cone search for each star with epoch adjustment
-queries = ["""
-SELECT *
-FROM gaiadr2.gaia_source
-WHERE 1=CONTAINS(
-        POINT('ICRS',gaiadr2.gaia_source.ra,gaiadr2.gaia_source.dec),
-        CIRCLE('ICRS', 
-               COORD1(EPOCH_PROP_POS({0},{1},{2},{3},{4},{5},2000,2015.5)),
-               COORD2(EPOCH_PROP_POS({0},{1},{2},{3},{4},{5},2000,2015.5)),
-               {6}))
-""".format(*simbad_vals[i], radius.value) for i in range(len(stars))]
-
-# Perform each query and get the result in a table
-jobs = [Gaia.launch_job_async(query) for query in queries]
-results = [job.get_results() for job in jobs]
