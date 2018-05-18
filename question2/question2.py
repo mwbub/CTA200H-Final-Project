e9aa474fd8b115246e71163df6216668fc205cd6
Mathew Bub <mathew.bub@gmail.com>
2018-05-18 17:35:01 -0400

    Added density plot

--- question2/question2.py
+++ question2/question2.py
@@ -6,23 +6,28 @@ import gaia_tools.load as load
 import gaia_tools.xmatch as xmatch
 from galpy.orbit import Orbit
 from galpy.potential import MWPotential2014
+from galpy.util.bovy_conversion import time_in_Gyr
 
 # Load the APOGEE red clump catalogue
 apogeerc = load.apogeerc()
 
 print("Loading data...")
 
+if not os.path.exists("data"):
+    os.mkdir("data")
+
 # Cross match APOGEE red clump with Gaia DR2
-if not os.path.exists("apogeerc_xmatched.csv"):
+if not os.path.exists("data/apogeerc_xmatched.csv"):
     xmatched = xmatch.cds(apogeerc, xcat = "vizier:I/345/gaia2", 
-                          savefilename = "apogeerc_xmatched.csv")[0]
+                          savefilename = "data/apogeerc_xmatched.csv")[0]
 else:
-    xmatched = xmatch.cds_load("apogeerc_xmatched.csv")
+    xmatched = xmatch.cds_load("data/apogeerc_xmatched.csv")
 
 # Filter out stars with no radial velocity
-rv_mask = [xmatched['radial_velocity'] != -9999.99]
+rv_mask = (xmatched['radial_velocity'] != -9999.99)
 xmatched_rv = xmatched[rv_mask]
 
+nstars = len(xmatched_rv)
 ra = xmatched_rv['ra']
 dec = xmatched_rv['dec']
 d = 1/xmatched_rv['parallax']
@@ -30,35 +35,40 @@ pmra = xmatched_rv['pmra']
 pmdec = xmatched_rv['pmdec']
 rv = xmatched_rv['radial_velocity']
 
-nsteps = 100
-physical = False
+nsteps = 1000
 t = np.linspace(0, 20, nsteps)
-coord_path = "integrated_{}_steps".format(nsteps)
+coord_path = "data/integrated_{}_steps".format(nsteps)
 
+physical = False
 if physical:
     t *= u.Gyr
     coord_path += "_physical"
+else:
+    t /= time_in_Gyr(220., 8.)
     
 coord_path += ".npy"
 
 if not os.path.exists(coord_path):
     print("Generating orbits...")
     orbits = [Orbit(vxvv = [ra[i], dec[i], d[i], pmra[i], pmdec[i], rv[i]], 
-                radec = True) for i in range(len(xmatched_rv))]
+                    ro = 8., vo = 220., radec = True) for i in range(nstars)]
     
     print("Integrating orbits...")
     for orbit in orbits:
         orbit.integrate(t, MWPotential2014)
         
     print("Getting coordinates...")
-    x = [[orbit.x(time) for orbit in orbits] for time in t]
-    y = [[orbit.y(time) for orbit in orbits] for time in t]
+    x = np.stack([orbit.x(t) for orbit in orbits], axis = 1)
+    y = np.stack([orbit.y(t) for orbit in orbits], axis = 1)
     np.save(coord_path, (x,y))
 else:
     x, y = np.load(coord_path)
 
 print("Plotting...")
-plt.scatter(x[-1], y[-1], s = 0.1)
-plt.xlim(-30,30)
-plt.ylim(-30,30)
+x_coords = x.flatten()
+y_coords = y.flatten()
+H, xedges, yedges = np.histogram2d(x_coords, y_coords, bins = 500, 
+                                   range = ((-15, 15), (-15, 15)))
+X, Y = np.meshgrid(xedges, yedges)
+plt.pcolormesh(X, Y, H, cmap = 'plasma')
 plt.show()
