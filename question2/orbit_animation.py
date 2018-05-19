85662cd824be264ccef0844e7af0b7e23a48f5b2
Mathew Bub <mathew.bub@gmail.com>
2018-05-19 15:10:59 -0400

    Added labels

--- question2/orbit_animation.py
+++ question2/orbit_animation.py
@@ -22,11 +22,11 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.set(xlim = (-30, 30), ylim = (-30, 30), xlabel = "x (kpc)", 
        ylabel = "y (kpc)", title = "APOGEE Red Clump Integrated Over 20 Gyr")
-scatter = ax.scatter(x[0], y[0], s=0.1)    
+scatter = ax.scatter(x[0], y[0], s=0.1)   
 
 def animate(i):
     scatter.set_offsets(np.c_[x[i], y[i]])
-    
+
 anim = animation.FuncAnimation(fig, animate, interval=100, frames=len(x)-1)
 plt.draw()
 plt.show()
